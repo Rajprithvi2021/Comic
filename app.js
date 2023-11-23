88507ee954be366dec23866edfdbeb3bd1a93b05
Rajprithvi2021 <96873307+Rajprithvi2021@users.noreply.github.com>
2023-11-23 18:22:55 +0530

    JavaScript Added

---
+++ app.js
@@ -0,0 +1,40 @@
+
+async function query(data) {
+	const response = await fetch(
+		"https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud",
+		{
+			headers: { 
+				"Accept": "image/png",
+				"Authorization": "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM", 
+				"Content-Type": "application/json" 
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+
+let btn = document.getElementById('btn');
+
+
+btn.addEventListener('click',()=>{
+    for(let i=1;i<=10;i++){
+    let val = document.getElementById(`div${i}`).value;
+    console.log(val);
+    query({"inputs": val}).then((response) => {
+        // Use image
+        console.log("working1");
+        let imgURL = URL.createObjectURL(response);
+        let img=document.createElement('img');
+        img.src=imgURL;
+        console.log(img);
+		document.getElementById('gallery').style.display='flex';
+        document.querySelector(`#box-${i}`).appendChild(img);
+
+    });
+    }
+    
+});
+
